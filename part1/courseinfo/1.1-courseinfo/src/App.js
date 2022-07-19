b369f51243533dbc4b8e39b3844db9dd70fee126
Jaathavan <rjaathu@outlook.com>
2022-07-19 16:17:38 -0400

    modify header and content component to work

--- part1/courseinfo/1.1-courseinfo/src/App.js
+++ part1/courseinfo/1.1-courseinfo/src/App.js
@@ -2,17 +2,7 @@ const Header = (props) => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <h1>{props.course}</h1>
     </div>
   )
 }
@@ -21,17 +11,15 @@ const Content = (props) => {
 
   return (
     <div>
-      <h1>{course}</h1>
       <p>
-        {part1} {exercises1}
+        {props.part1} {props.exercises1}
       </p>
       <p>
-        {part2} {exercises2}
+        {props.part2} {props.exercises2}
       </p>
       <p>
-        {part3} {exercises3}
+        {props.part3} {props.exercises3}
       </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
@@ -40,16 +28,6 @@ const Total = (props) => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
@@ -67,7 +45,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
       <Total />
     </div>
   )

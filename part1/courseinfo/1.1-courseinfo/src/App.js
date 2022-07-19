2b1e7814de09eaa84e0c007f06335bf92f7f0255
Jaathavan <rjaathu@outlook.com>
2022-07-19 16:09:58 -0400

    add new app and header component

--- part1/courseinfo/1.1-courseinfo/src/App.js
+++ part1/courseinfo/1.1-courseinfo/src/App.js
@@ -1,11 +1,4 @@
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+const Header = (props) => {
 
   return (
     <div>
@@ -20,6 +13,22 @@ const App = () => {
         {part3} {exercises3}
       </p>
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
     </div>
   )
 }

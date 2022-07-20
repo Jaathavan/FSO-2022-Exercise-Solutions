9082fb491e82264c5de210006f47dfc4dfcbe9e0
Jaathavan <rjaathu@outlook.com>
2022-07-20 11:30:39 -0400

    refactor Totalt component in App Component to accpet correct values + complete exercise

--- part1/courseinfo/1.4-courseinfo/src/App.js
+++ part1/courseinfo/1.4-courseinfo/src/App.js
@@ -80,7 +80,7 @@ const App = () => {
     <div>
       <Header course={course} />
       <Content part1={parts[0].name} exercises1={parts[0].exercises} part2={parts[1].name} exercises2={parts[1].exercises} part3={parts[2].name} exercises3={parts[2].exercises}/>
-      <Total exercises1={parts1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+      <Total exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises} />
     </div>
   )
 }

47ed2003c9c8ce8c22dbd9bbb1b8764f64dd28a5
Jaathavan <rjaathu@outlook.com>
2022-07-28 17:03:16 -0400

    remove errors by adding unique key

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -63,11 +63,11 @@ const Course = ( {courses} ) => {
 
   return (
     <div>
-        {courses.map(course =>
+        {courses.map((course, index) =>
           <div>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
+            <Header key={course.id} course={course.name} />
+            <Content key={course.id+=3} parts={course.parts} />
+            <Total key={course.id+=5} parts={course.parts} />
           </div>
         )}
     </div>

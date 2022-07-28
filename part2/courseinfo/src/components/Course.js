8927237baf55098be4d5e46902148cf2107fa7c1
Jaathavan <rjaathu@outlook.com>
2022-07-28 17:19:50 -0400

    change Course Component into seperate repositiory + info + complete 2.5

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,13 +1,49 @@
 const Course = ( {courses} ) => {
     return (
       <div>
-          {courses.map((course, index) =>
+          {courses.map(course =>
             <div>
               <Header key={course.id} course={course.name} />
               <Content key={course.id+=3} parts={course.parts} />
               <Total key={course.id+=5} parts={course.parts} />
             </div>
           )}
+      </div>
+    )
+}
+const Header = ( {course} ) => {
+    return (
+      <div>
+        <h2>{course}</h2>
+      </div>
+    )
+}
+  
+const Part = ( {part, exercises} ) => {
+    return (
+      <div>
+        <p>
+          {part} {exercises}
+        </p>
+      </div>
+    )
+}
+  
+const Content = ( {parts} ) => {
+    return (
+      <div>
+        {parts.map(part => 
+          <Part key={part.id} part={part.name} exercises={part.exercises} />
+        )}
+      </div>
+    )
+}
+  
+const Total = ( {parts} ) => {
+    const total = parts.reduce((sum, num) => sum + num.exercises, 0)
+    return (
+      <div>
+        <p><strong>Total of {total} exercises</strong></p>
       </div>
     )
 }

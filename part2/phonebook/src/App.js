4a38f00e6cd601ee91619b20a2aaf42f021f6fb6
Jaathavan <rjaathu@outlook.com>
2022-07-29 15:33:31 -0400

    add test data + complete JSX for 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,7 +2,10 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: 1234567890 }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
@@ -37,6 +40,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input />
+      </div>
+      <h2>Add a New</h2>
       <form onSubmit={addDetails}>
         <div>
           name: <input value={newName} onChange={handleNameChange} required/>

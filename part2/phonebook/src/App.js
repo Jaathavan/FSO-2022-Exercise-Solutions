044f08f4b8228b4aa45e49af90469446d2b6a28f
Jaathavan <rjaathu@outlook.com>
2022-07-29 14:35:39 -0400

    use event handler to displaychange in name

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,19 +6,26 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => person.name)}
+      {persons.map(person => 
+        <div key={person.name}>{person.name}</div>
+      )}
     </div>
   )
 }

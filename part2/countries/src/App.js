7d392f954a6ecdd8d853c4bf02c7722e817a423e
Jaathavan <rjaathu@outlook.com>
2022-08-04 11:10:31 -0400

    add imports and states

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,7 +1,15 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const App = () => {
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
+
+
   return (
     <div>
-      <div>Find Countries: </div>
+      <div>Find Countries: <input></input></div>
+      
     </div>
   );
 }

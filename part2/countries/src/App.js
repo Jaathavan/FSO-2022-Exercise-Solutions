576ed4bd8d749f97ccd2faf2460e5125f361e75d
Jaathavan <rjaathu@outlook.com>
2022-08-04 11:17:44 -0400

    create useEffect hook to retrieve data from src

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -5,6 +5,13 @@ const App = () => {
   const [countries, setCountries] = useState([])
   const [filter, setFilter] = useState('')
 
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        setCountries(response.data)
+      })
+  }, [])
 
   return (
     <div>

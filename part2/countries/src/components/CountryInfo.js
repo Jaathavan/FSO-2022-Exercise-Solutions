12abcacb38d548ad4799d656611b564b904e591d
Jaathavan <rjaathu@outlook.com>
2022-08-04 17:22:32 -0400

    map over languages vakues + complete 2.12

--- part2/countries/src/components/CountryInfo.js
+++ part2/countries/src/components/CountryInfo.js
@@ -1,6 +1,6 @@
 const CountryInfo = ( { country } ) => {
-    
-    console.log("C", country[0])
+    const languages = Object.values(country[0].languages)
+
     return (
         <div>
             <h2>{country[0].name.common}</h2>
@@ -8,7 +8,8 @@ const CountryInfo = ( { country } ) => {
             <div>Area: {country[0].area}</div>
             
             <h3>Languages: </h3>
-            
+            {languages.map(l => <li key={l}>{l}</li>)}
+            <br></br>
             <img src={country[0].flags.svg}></img>
         </div>
     )

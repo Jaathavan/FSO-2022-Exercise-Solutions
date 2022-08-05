1d7964c357c7894ed30c6020692b4434c69e7f08
Jaathavan <rjaathu@outlook.com>
2022-08-04 22:17:33 -0400

    conditonally render

--- part2/countries/src/components/Weather.js
+++ part2/countries/src/components/Weather.js
@@ -1,23 +1,32 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const Weather = ( { city } ) => {
-    const [weather, setWeather] = useState([])
+const Weather = ({ city }) => {
+    const [weather, setWeather] = useState([]);
+    const api_key = process.env.REACT_APP_API_KEY;
+
     useEffect(() => {
         axios
-          .get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2a287d39bd4da6bf5c24aa61c6a91700&units=metric`)
+          .get(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${api_key}`
+          )
           .then((response) => {
-            setWeather(response.data)
-         })
-    }, [])
+            setWeather(response.data);
+         });
+    }, []);
 
+    //weather.main statement is required to conditionally render so that the info is not rendered when the state is undefined
     return (
-        <div>
-            <h2>Weather in {city}</h2>
-            <div>Temperature: {weather.main.temp} °C</div>
-
-            <div>Wind: {weather.wind.speed} m/s</div>
-        </div>
+        <>
+            {weather.main ? (
+                <div>
+                    <h2>Weather in {city}</h2>
+                    <div>Temperature: {weather.main.temp} °C</div>
+                    <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
+                    <div>Wind: {weather.wind.speed} m/s</div>
+                </div>
+            ) : null}
+        </>
     )
 }
 

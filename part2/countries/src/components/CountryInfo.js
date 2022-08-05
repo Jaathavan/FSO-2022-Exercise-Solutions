c1b2fc2e1bd26a86c3c4aad8b9b33580929a5277
Jaathavan <rjaathu@outlook.com>
2022-08-04 20:45:18 -0400

    move to seperate component

--- part2/countries/src/components/CountryInfo.js
+++ part2/countries/src/components/CountryInfo.js
@@ -1,18 +1,7 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
+import Weather from "./Weather"
 
 const CountryInfo = ( { country } ) => {
-    const [weather, setWeather] = useState([])
     const languages = Object.values(country[0].languages)
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Ottawa&appid=2a287d39bd4da6bf5c24aa61c6a91700&units=metric"
-    
-    useEffect(() => {
-        axios
-        .get(`https://api.openweathermap.org/data/2.5/weather?q=${country[0].capital}&appid=2a287d39bd4da6bf5c24aa61c6a91700&units=metric`)
-        .then(response => {
-            setWeather(response.data)
-        })
-    })
 
     return (
         <div>
@@ -24,11 +13,7 @@ const CountryInfo = ( { country } ) => {
             {languages.map(l => <li key={l}>{l}</li>)}
             <br></br>
             <img src={country[0].flags.svg}></img>
-
-            <h2>Weather in {country[0].capital}</h2>
-            <div>Temperature: </div>
-
-            <div>Wind: </div>
+            <Weather city={country[0].capital} />
         </div>
     )
 }
---
+++ part2/countries/src/components/Weather.js
@@ -0,0 +1,23 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+const Weather = ( { city } ) => {
+    const [weather, setWeather] = useState([])
+    useEffect(() => {
+        axios
+         .get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2a287d39bd4da6bf5c24aa61c6a91700&units=metric`)
+         .then(response => {
+            setWeather(response.data)
+         })
+    })
+    return (
+        <div>
+            <h2>Weather in {city}</h2>
+            <div>Temperature: {weather.main.temp} °C</div>
+
+            <div>Wind: {weather.wind.speed} m/s</div>
+        </div>
+    )
+}
+
+export default Weather

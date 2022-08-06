24929ccff41834be63c73c07a86591bf8ef39990
Jaathavan <rjaathu@outlook.com>
2022-08-06 09:49:11 -0400

    allow user to update number + complete 2.18

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -38,6 +38,18 @@ const App = () => {
     }
   }
 
+  const updateNumber = (id) => {
+    const person = persons.find(p => p.id === id)
+    const changedPerson = { ...person, number: newNumber }
+    personService
+      .update(id, changedPerson)
+      .then(returnedPerson => {
+        setPersons(persons.map(person => person.id !== id ? person : returnedPerson))
+        setNewName('')
+        setNewNumber('')
+      })
+  }
+
   const addDetails = (event) => {
     event.preventDefault()
     const nameObject = {
@@ -45,10 +57,10 @@ const App = () => {
       number: newNumber
     }
     if ((persons.filter(person => person.name === newName)).length > 0) {
-      alert(`${newName} is already added to phonebook`)
-    }
-    else if ((persons.filter(person => person.number === newNumber)).length > 0) {
-      alert(`${newNumber} is already added to phonebook`)
+      if(window.confirm(`${newName} is already added to phonebook, replace older number with new number?`)) {
+        let person = persons.find(p => p.name === newName)
+        updateNumber(person.id)
+      }
     }
     else {
       personService
